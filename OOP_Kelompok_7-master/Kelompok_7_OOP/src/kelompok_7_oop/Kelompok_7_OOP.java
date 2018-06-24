7d5809299b92cb1b24a6df1d7988ef1eb164d894
Novia <16.9335@stis.ac.id>
2018-06-24 11:54:00 +0700

    commit pertama

---
+++ OOP_Kelompok_7-master/Kelompok_7_OOP/src/kelompok_7_oop/Kelompok_7_OOP.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kelompok_7_oop;
+
+/**
+ *
+ * @author Nashir
+ */
+public class Kelompok_7_OOP {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
