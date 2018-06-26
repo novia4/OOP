59f700d475ef935af9601c9a4554e6d1e5b4b911
DayKharisma <16.9066@stis.ac.id>
2018-06-26 23:17:41 +0700

    class day

---
+++ OOP_Kelompok_7-master/Kelompok_7_OOP/src/kelompok_7_oop/Pekerjaan.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kelompok_7_oop;
+
+/**
+ *
+ * @author maya
+ */
+public class Pekerjaan {
+    private String pekerjaan;
+    private String jenisPekerjaan;
+    private int lamaBekerja;
+    private int lamaMencari;
+    private JamKerja jamkerja;
+    private String statusPekerjaan;
+    private String caraPembukuan;
+    private String sistemPengupahan;
+    private FasilitasPekerjaan layananPerusahaan;
+    private String perjanjianKerja;
+    private String serikatPekerja;
+    private String jenisInstansi;
+    private String lokasiUtamaKerja;
+    private Lokasi tempatKerja;
+    private String frekuensiPergiKerja;
+    private String jarak;
+    private String lamaPerjalanan;
+    private String jenisTransportasi;
+}
---
+++ OOP_Kelompok_7-master/Kelompok_7_OOP/src/kelompok_7_oop/PengalamanKerja.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kelompok_7_oop;
+
+/**
+ *
+ * @author maya
+ */
+public class PengalamanKerja {
+    private String pekerjaanSebelumnya;
+    private String pernahBerhentiBekerja;
+    private String alasanBerhentiBekerja;
+    private String lapUsahaTerakhir;
+    private String statusTerakhir;
+    private String bekerjaDiLuarNegeri;
+}
