ee90ad0c7ac48bd592fd6aaca38c83b4dd85aeca
zweilo <dyntaadiasa@gmail.com>
2018-06-27 19:54:02 +0700

    ini yg kemaren

---
+++ OOP_Kelompok_7-master/Kelompok_7_OOP/src/kelompok_7_oop/Gangguan.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package kelompok_7_oop;
+
+/**
+ *
+ * @author ACER
+ */
+public class Gangguan {
+    private String penglihatan;
+    private String pendengaran;
+    private String mobilitas;
+    private String gerakkanJariAtauTngn;
+    private String komunikasi;
+    private String lainnya;
+    
+    public String getPenglihatan{
+        return penglihatan;    
+    }
+    
+    public void setPenglihatan(String penglihatan) {
+        this.penglihatan = penglihatan;
+    }
+    
+    public String getPendengaran{
+        return pendengaran;    
+    }
+    
+    public void setPendengaran(String pendengaran) {
+        this.pendengaran = pendengaran;
+    }
+    
+    public String getMobilitas{
+        return mobilitas;    
+    }
+    
+    public void setMobilitas(String mobilitas) {
+        this.mobilitas = mobilitas;
+    }
+    
+    public String getGerakkanJariAtauTngn{
+        return gerakkanJariAtauTngn;    
+    }
+    
+    public void setGerakkanJariAtauTngn(String gerakkanJariAtauTngn) {
+        this.gerakkanJariAtauTngn = gerakkanJariAtauTngn;
+    }
+    
+    public String getKomunikasi{
+        return komunikasi;    
+    }
+    
+     public void setKomunikasi(String komunikasi) {
+        this.komunikasi = komunikasi;
+    }
+    
+    
+    public String getLainnya{
+        return lainnya;    
+    }
+    
+     public void setLainnya(String lainnya) {
+        this.lainnya = lainnya;
+    }
+    
+    
+}
---
+++ OOP_Kelompok_7-master/Kelompok_7_OOP/src/kelompok_7_oop/KegiatanMencariKerja.java
@@ -0,0 +1,114 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package kelompok_7_oop;
+
+/**
+ *
+ * @author ACER
+ */
+public class KegiatanMencariKerja {
+    private String mencariKerja;
+    private String mempersiapkanUsaha;
+    private int lamaMencariKerja;
+    private ArrayList alasanMencariKerja;
+    private ArrayList upayaMencariKerja;
+    private ArrayList alasanTdkMncariKerja;
+    private String mulaiPekerjaan;
+    private String menerimaTawaran;
+    private String siapKerja;
+    private String inginPekerjaan;
+    private Array upah;
+    
+    public String getMencariKerja{
+        return mencariKerja;    
+    }
+    
+    public void setMencariKerja(String mencariKerja) {
+        this.mencariKerja = mencariKerja;
+    }
+    
+    public String getMempersiapkanUsaha{
+        return mempersiapkanUsaha;    
+    }
+    
+    public void setMempersiapkanUsaha(String mempersiapkanUsaha) {
+        this.mempersiapkanUsaha = mempersiapkanUsaha;
+    }
+    
+    public int getLamaMencariKerja{
+        return lamaMencariKerja;    
+    }
+    
+    public void setLamaMencariKerja(String LamaMencariKerja) {
+        this.lamaMencariKerja = lamaMencariKerja;
+    }
+    
+      public ArrayList getAlasanMencariKerja{
+        return alasanMencariKerja;    
+    }
+    
+    public void setAlasanMencariKerja(String alasanMencariKerja) {
+        this.alasanMencariKerja = alasanMencariKerja;
+    }
+    
+      public ArrayList getUpayaMencariKerja{
+        return upayaMencariKerja;    
+    }
+    
+    public void setUpayaMencariKerja(String upayaMencariKerja) {
+        this.upayaMencariKerja = upayaMencariKerja;
+    }
+    
+      public ArrayList getAlasanTdkMencariKerja{
+        return alasanTdkMencariKerja;    
+    }
+    
+    public void setAlasanTdkMencariKerja(String alasanTdkMencariKerja) {
+        this.alasanTdkMencariKerja = alasanTdkMencariKerja;
+    }
+    
+    public String getMulaiPekerjaan{
+        return mulaiPekerjaan;    
+    }
+    
+    public void setMulaiUsaha(String mulaiUsaha) {
+        this.mulaiUsaha = mulaiUsaha;
+    }
+    
+    public String getMenerimaTawaran{
+        return menerimaTawaran;    
+    }
+    
+    public void setMenerimaTawaran(String menerimaTawaran) {
+        this.menerimaTawaran = menerimaTawaran;
+    }
+    
+      public String getSiapKerja{
+        return siapKerja;    
+    }
+    
+    public void setSiapKerja(String siapKerja) {
+        this.siapKerja = siapKerja;
+    }
+    
+      public String getInginPekerjaan{
+        return inginPekerjaan;    
+    }
+    
+    public void setInginPekerjaan(String inginPekerjaan) {
+        this.inginPekerjaan = inginPekerjaan;
+    }
+    
+       public Array getUpah{
+        return upah;    
+    }
+    
+    public void setUpah(Array upah) {
+        this.upah = upah;
+    }
+    
+}
